5dbeae125eb73b82d0a5e9840c123ab4273bbbad
redfoxxll <xinglinlin.angelina@gmail.com>
2014-11-24 22:32:04 +0900

    stageloop

---
+++ Assets/_Script/StageLoop.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageLoop : MonoBehaviour {
+	public float StageOffset;
+	public Transform stage;
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag != "StageLoop")
+						return;
+
+		Instantiate (stage, new Vector3 (other.transform.position.x, other.transform.position.y, other.transform.position.z + StageOffset),other.transform.rotation);
+	}
+}
