7215370157cc669e542c7aa1c36c40f6af23656f
redfoxxll <xinglinlin.angelina@gmail.com>
2014-11-27 02:43:41 +0900

    gai

--- Assets/_Script/BallDestroy.cs
+++ Assets/_Script/BallDestroy.cs
@@ -2,14 +2,14 @@
 using System.Collections;
 
 public class BallDestroy : MonoBehaviour {
-
+	public float downdelete;
 	// Use this for initialization
 	void Start () {
 		InvokeRepeating ("DestroyDetect",5.0f,0.1f);
 	}
 	void DestroyDetect()
 	{
-		if(transform.position.z < Camera.main.transform.position.z)
+		if(transform.position.z < Camera.main.transform.position.z||transform.position.y < downdelete)
 			GameObject.Destroy (transform.gameObject);
 	}
 }
